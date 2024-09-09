929f75fd76054a9adf1c1f0c8cf26f435b45fbaf
Lassa30 <loskutovvlad@gmail.com>
2024-09-09 22:10:38 +0300

    linear regression using SGD added

--- include/models/linear_regression.hpp
+++ include/models/linear_regression.hpp
@@ -10,134 +10,104 @@
 #include <string>
 #include <vector>
 
-namespace mlfs
-{
-    // Linear Regression with no regularization and MSE as a loss function.
-    class LinearRegressionSGD
-    {
-    public:
-        LinearRegressionSGD() = default;
-        ~LinearRegressionSGD() = default;
+namespace mlfs {
+// Linear Regression with no regularization and MSE as a loss function.
+class LinearRegressionSGD {
+public:
+  LinearRegressionSGD() = default;
+  ~LinearRegressionSGD() = default;
 
-        LinearRegressionSGD(const std::size_t &batch, const double &learningRate, const int &epochs)
-            : lr_{learningRate}, batch_{batch}, epochs_{epochs}
-        {
-        }
+  LinearRegressionSGD(const std::size_t &batch, const double &learningRate,
+                      const int &epochs)
+      : learning_rate_{learningRate}, batch_{batch}, epochs_{epochs} {}
 
-        void train(const Matrix &features, const Matrix &target, int randomState = 42)
-        {
-            // random
-            std::mt19937 gen(randomState);
-            std::uniform_int_distribution<> intDis(0, features.rows_ - 1);
-            std::uniform_real_distribution<> realDis(-1, 1);
+  void train(Matrix &features, Matrix &target, int randomState = 42) {
+    // random
+    std::mt19937 gen(randomState);
+    std::uniform_int_distribution<> intDis(0, features.rows() - 1);
+    std::uniform_real_distribution<> realDis(-1, 1);
 
-            // preparation
-            batch_ = std::min(batch_, features.rows_);
-            // std::vector<double> randVect(features.cols_);
-            // for (auto &elem : randVect)
-            // {
-            //     elem = realDis(gen);
-            // }
-            weights_ = Matrix(1, features.cols_, std::vector<double>(features.cols_, 0));
-            bias_ = 0.0;
+    // preparation
+    batch_ = std::min(batch_, features.rows());
+    weights_ =
+        Matrix(1, features.cols(), std::vector<double>(features.cols(), 0));
+    bias_ = 0.0;
 
-            // SGD
+    // SGD
+    for (int e = 0; e < epochs_; ++e) {
+      for (auto i = 0; i < features.rows(); i += batch_) {
+        std::vector<int> idx(batch_);
+        std::generate(idx.begin(), idx.end(), [&]() { return intDis(gen); });
 
-            for (int e = 0; e < epochs_; ++e)
-            {
-                for (auto i = 0; i <= features.rows_; i += batch_)
-                {
-                    // full rewrite needed
-                }
-            }
-        }
+        Matrix featuresBatch = getBatch(features, idx);
+        Matrix targetBatch = getBatch(target, idx);
 
-        Matrix predict(const Matrix &features)
-        {
-            // nothing to predict
-            // if (!isFitted_)
-            // {
-            //     throw std::logic_error(
-            //         "Linear Regression: Couldn't predict, the model isn't fitted\n");
-            // }
-            auto mat = features.matmul(weights_.T());
-            mat += bias_;
+        Matrix dw =
+            gradMSE(targetBatch, featuresBatch); // Gradient w.r.t. weights
+        double db =
+            biasGradMSE(targetBatch, featuresBatch); // Gradient w.r.t. bias
 
-            return mat;
-        }
+        weights_ -= dw * learning_rate_; // weights update
+        bias_ -= db * learning_rate_;    // bias update
+      }
+    }
+  }
 
-        double score(const Matrix &prediction, const Matrix &target)
-        {
-            return ((target - prediction) * (target - prediction) / target.rows_).sum();
-        }
+  Matrix predict(const Matrix &features) {
+    return features.matmul(weights_.T()) + bias_;
+  }
 
-        void printWeights() const
-        {
-            std::cout << "\nWEIGHTS\n";
-            weights_.print_matrix();
-            std::cout << "\nBIAS\n";
-            std::cout << bias_ << std::endl;
-        }
+  double score(const Matrix &prediction, const Matrix &target) {
+    return ((target - prediction) * (target - prediction) / target.rows())
+        .sum();
+  }
 
-    private:
-        double lr_ = 0.001;
-        std::size_t batch_ = 10;
-        int epochs_ = 10;
-        bool isFitted_ = false;
+  void printWeights() const {
+    std::cout << "\nWEIGHTS:\n";
+    weights_.print_matrix();
 
-        Matrix weights_{};
-        double bias_{};
+    std::cout << "\nBIAS:\n";
+    std::cout << bias_ << std::endl;
+  }
 
-        int randomState_ = 42;
+private:
+  double learning_rate_ = 0.001;
+  std::size_t batch_ = 10;
+  int epochs_ = 10;
 
-        // SGD
-        Matrix MSE(const Matrix &y, const Matrix &X, const Matrix &w_0, const double &b) const
-        {
-            auto prediction = std::move(X.matmul(w_0.T()) + b);
-            return (y - prediction) * (y - prediction) / X.rows_;
-        }
+  Matrix weights_{};
+  double bias_{};
 
-        // MSE differentiation w.r.t. weights_
-        Matrix gradMSE(const Matrix &y, const Matrix &X) const
-        {
-            // dw = -(2 / len(y_batch)) * np.dot(X_batch.T, (y_batch - y_pred))
-            // db = -(2 / len(y_batch)) * np.sum(y_batch - y_pred)
+  int randomState_ = 42;
 
-            auto y_pred = X.matmul(weights_.T()) + bias_;
-            auto XT = X.T();
-            auto diff = y - y_pred;
-            auto gradScaled = XT.matmul(diff) * (-2.0) / batch_;
+  // MSE differentiation w.r.t. weights_
+  Matrix gradMSE(const Matrix &y, const Matrix &X) const {
+    // dw = -(2 / len(y_batch)) * np.dot(X_batch.T, (y_batch - y_pred))
+    // db = -(2 / len(y_batch)) * np.sum(y_batch - y_pred)
+    auto y_pred = X.matmul(weights_.T()) + bias_;
+    auto grad = (X.T().matmul(y - y_pred)) * -2.0 / batch_;
 
-            return gradScaled.T();
-        }
+    return grad.T();
+  }
 
-        double biasGradMSE(const Matrix &y, const Matrix &X) const
-        {
-            auto y_pred = X.matmul(weights_.T()) + bias_;
+  double biasGradMSE(const Matrix &y, const Matrix &X) const {
+    auto y_pred = X.matmul(weights_.T()) + bias_;
 
-            auto gradScaled = (y - y_pred) * (-2.0) / batch_;
+    return ((y - y_pred) * (-2.0) / batch_).sum();
+  }
 
-            return gradScaled.sum();
-        }
+  Matrix getBatch(Matrix &mat, const std::vector<int> &idx) const {
+    std::vector<double> resVect;
+    for (auto rowInd : idx) {
+      auto rowVect = mat.get_row(rowInd).get_data();
+      resVect.insert(resVect.end(), rowVect.begin(), rowVect.end());
+    }
 
-        Matrix getBatch(const Matrix &mat, const std::vector<int> &idx) const
-        {
-            std::vector<double> resVect;
-            for (auto rowInd : idx)
-            {
-                auto rowVect = mat.get_row(rowInd).get_data();
-                resVect.insert(resVect.end(), rowVect.begin(), rowVect.end());
-            }
-
-            if (resVect.size() == batch_ * mat.cols_)
-            {
-                return Matrix(batch_, mat.cols_, resVect);
-            }
-
-            else
-            {
-                throw std::runtime_error("getBatch():\n\tVect size don't match...\n");
-            }
-        }
-    };
-}
+    if (resVect.size() == batch_ * mat.cols()) {
+      return Matrix(batch_, mat.cols(), resVect);
+    } else {
+      throw std::runtime_error("getBatch():\n\tVect size don't match...\n");
+    }
+  }
+};
+} // namespace mlfs
--- tests/linear_regression_test.cpp
+++ tests/linear_regression_test.cpp
@@ -1,33 +1,34 @@
+#include <iomanip>
 #include <models/linear_regression.hpp>
 #include <utils/matrix.hpp>
 #include <utils/utils.hpp>
 
-int main()
-{
-    std::vector<double> data;
-    mlfs::utils::dataFromCsv(data, "../tests/winequality-white.csv", ';');
+int main() {
 
-    std::vector<double> X;
-    std::vector<double> y;
+  std::vector<double> data;
+  mlfs::utils::dataFromCsv(data, "../tests/winequality-white.csv", ';');
 
-    for (auto i = 0; i < data.size(); i++)
-    {
-        if (i % 12 != 11)
-            X.push_back(data[i]);
-        else
-            y.push_back(data[i]);
-    }
+  std::cout << std::fixed << std::setprecision(5);
 
-    
-    mlfs::Matrix designMatrix(4898, 11, X);
-    mlfs::Matrix targetColumn(4898, 1, y);
+  std::vector<double> X;
+  std::vector<double> y;
 
-    mlfs::LinearRegressionSGD LinReg(16, 1e-5, 1);
+  for (auto i = 0; i < data.size(); i++) {
+    if (i % 12 != 11)
+      X.push_back(data[i]);
+    else
+      y.push_back(data[i]);
+  }
 
-    LinReg.train(designMatrix, targetColumn, 42);
+  mlfs::Matrix designMatrix(4898, 11, X);
+  mlfs::Matrix targetColumn(4898, 1, y);
 
-    auto prediction = LinReg.predict(designMatrix);
+  mlfs::LinearRegressionSGD LinReg(256, 1e-5, 1000);
 
-    std::cout << "\nSCORE:\n\t";
-    std::cout << LinReg.score(prediction, targetColumn) << std::endl;
+  LinReg.train(designMatrix, targetColumn, 42);
+
+  auto prediction = LinReg.predict(designMatrix);
+
+  std::cout << "\nSCORE:\n\t";
+  std::cout << LinReg.score(prediction, targetColumn) << std::endl;
 }
